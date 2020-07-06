a2b88f5f8338d0c6c9b7752d32cd040c928fbaf7
ErnieBuyco <buycoernie@gmail.com>
2020-07-06 08:21:51 +0800

    docker-setup

--- query/index.js
+++ query/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { default: Axios } = require('axios');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
